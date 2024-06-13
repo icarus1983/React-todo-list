7012a514f3bc766af2bbbd1b5aa018e96842fc6c
Guillermo Vergara <guiver83@gmail.com>
2024-06-13 08:28:54 -0400

    Trabajando en Components

---
+++ src/Components/Headers.js
@@ -0,0 +1,10 @@
+import { Header as Head, Icon } from "semantic-ui-react"
+
+export default function Header() {
+    return(
+        <div className="header">
+            
+        </div>
+    );
+
+}
